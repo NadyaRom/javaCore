2d77e873490f0e1aec3ca6b11c89388f9ed5d06c
nadya <nadya@nadya>
2017-02-21 15:35:48 +0200

    iterator

---
+++ JavaCore/src/collection_list_iterator/Car.java
@@ -0,0 +1,103 @@
+package collection_list_iterator;
+
+public class Car {
+	
+	private String name;
+	private Wheel wheel;
+	private Body body;
+	private Helm helm;
+	
+	public Car(String name, Wheel wheel, Body body, Helm helm) {
+		this.name = name;
+		this.wheel = wheel;
+		this.body = body;
+		this.helm = helm;
+	}
+
+	public Car() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Wheel getWheel() {
+		return wheel;
+	}
+
+	public void setWheel(Wheel wheel) {
+		this.wheel = wheel;
+	}
+
+	public Body getBody() {
+		return body;
+	}
+
+	public void setBody(Body body) {
+		this.body = body;
+	}
+
+	public Helm getHelm() {
+		return helm;
+	}
+
+	public void setHelm(Helm helm) {
+		this.helm = helm;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((body == null) ? 0 : body.hashCode());
+		result = prime * result + ((helm == null) ? 0 : helm.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((wheel == null) ? 0 : wheel.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Car other = (Car) obj;
+		if (body == null) {
+			if (other.body != null)
+				return false;
+		} else if (!body.equals(other.body))
+			return false;
+		if (helm == null) {
+			if (other.helm != null)
+				return false;
+		} else if (!helm.equals(other.helm))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (wheel == null) {
+			if (other.wheel != null)
+				return false;
+		} else if (!wheel.equals(other.wheel))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ JavaCore/src/collection_list_iterator/Main.java
@@ -0,0 +1,97 @@
+package collection_list_iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Main {
+
+	static List<Car> cars=new ArrayList<>();
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String [] arqs) {
+		cars.add(new Car("Honda", new Wheel(200), new Body("Red"), new Helm(false)));
+		cars.add(new Car("Toyota", new Wheel(190), new Body("Green"), new Helm(true)));
+		cars.add(new Car("Mercedes", new Wheel(215), new Body("Black"), new Helm(true)));
+		cars.add(new Car("Deaweo", new Wheel(190), new Body("Red"), new Helm(false)));
+		cars.add(new Car("Renault", new Wheel(210), new Body("Green"), new Helm(true)));
+		cars.add(new Car("Lexus", new Wheel(215), new Body("White"), new Helm(false)));
+		cars.add(new Car("Lada", new Wheel(195), new Body("White"), new Helm(true)));
+
+		method5();
+	}
+
+
+	//	Знайти машини, які використовують введений тип (колір) кузова.
+	public static void method1(){
+		System.out.println("Enter color of body: ");
+		String color = sc.next();
+		Iterator<Car> iterator = cars.iterator();
+		while (iterator.hasNext()) {
+			Car car = (Car) iterator.next();
+			if(car.getBody().getColor().equalsIgnoreCase(color)){
+				System.out.println(car);
+			}
+
+		}
+	}
+	//	Видалити з ліста усі машини, які мають введений колір кузова.
+	public static void method2(){
+		System.out.println("Enter color of body: ");
+		String color = sc.next();
+		Iterator<Car> iterator = cars.iterator();
+		while (iterator.hasNext()) {
+			Car car = (Car) iterator.next();
+			if(car.getBody().getColor().equalsIgnoreCase(color)){
+				iterator.remove();
+			}
+
+		}
+	}
+	
+	//	Замінити усім машинам, діаметр коліс яких лежить у введеному діапазоні колір на зелений.
+	public static void method3(){
+		System.out.println("Enter min diametr of wheel: ");
+		int min = sc.nextInt();
+		System.out.println("Enter max diametr of wheel: ");
+		int max=sc.nextInt();
+		int diam = sc.nextInt();
+		Iterator<Car> iterator = cars.iterator();
+		while (iterator.hasNext()) {
+			Car car = (Car) iterator.next();
+			if(car.getWheel().getDiametrWheel()>=min&&car.getWheel().getDiametrWheel()<=max){
+				car.getBody().setColor("Green");	
+		}	
+
+	}
+}
+	//	Видалити усі машини, які мають введений тип (колір) кузова та введений діапазон діаметра керма
+	public static void method4(){
+		System.out.println("Enter diametr of wheel: ");
+		int diam = sc.nextInt();
+		System.out.println("Enter color of body: ");
+		String color = sc.next();
+		Iterator<Car> iterator = cars.iterator();
+		while (iterator.hasNext()) {
+			Car car = (Car) iterator.next();
+			if(car.getBody().getColor().equalsIgnoreCase(color)&&car.getWheel().getDiametrWheel() == diam){
+				iterator.remove();	
+		}
+	}
+
+	}
+	
+	//	Запам'ятати індекси машин, які мають введений тип(колір) кузова
+	public static void method5(){
+		System.out.println("Enter color of body: ");
+		String color = sc.next();
+		Iterator<Car> iterator = cars.iterator();
+		while (iterator.hasNext()) {
+			Car car = (Car) iterator.next();
+			if(car.getBody().getColor().equalsIgnoreCase(color))
+				System.out.println(cars.indexOf(car));
+		}
+	}
+}
